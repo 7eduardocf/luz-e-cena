23bcf73315f387b2e3a81148d25981bdacbc6a16
7eduardocf <eduardocfrota901@gmail.com>
2025-05-19 22:08:57 -0400

    Feito o componente de Lista de filmes

---
+++ src/components/movieList/index.tsx
@@ -0,0 +1,32 @@
+import CardMovie from "../cardMovie"
+import styles from "./movieList.module.css"
+
+type Categoria = "2D" | "3D";
+type Censura = "Livre" | "10 anos" | "12 anos" | "14 anos" | "16 anos";
+
+interface Movie {
+    id: number
+    src: string;
+    alt: string;
+    titulo: string;
+    categoria: Categoria;
+    censura: Censura;
+    genero: string;
+    duracao: number;
+}
+
+interface MovieListProps{
+    movies: Movie[]
+}
+
+function MovieList({ movies }: MovieListProps) {
+    return (
+        <ul className={styles.lista}>
+            {movies.map((movie) => {
+                return <CardMovie key={movie.id} {...movie} />
+            })}
+        </ul>
+    )
+}
+
+export default MovieList
