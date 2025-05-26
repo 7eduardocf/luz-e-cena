3101b7b9c1cd13ae3ee6d1c3a89ece4e91d6b955
7eduardocf <eduardocfrota901@gmail.com>
2025-05-25 23:24:33 -0400

    Feita a criação dos hooks customizados useFetchMovies e useFilterMovies

--- src/components/movieSection/index.tsx
+++ src/components/movieSection/index.tsx
@@ -5,24 +5,28 @@ import InputText from "../inputText"
 import styles from "./movieSection.module.css"
 import MovieList from "../movieList"
 import useFetchMovies from "../../hooks/useFetchMovies"
+import useFilterMovies from "../../hooks/useFilterMovies"
 
 
 
 function MovieSection() {
     
     const {movies, error, isLoading} = useFetchMovies()
+    const {searchTerm, handleSearch, setSearchTerm, filteredMovies} = useFilterMovies(movies)
 
     return (
         <main>
             <section className={styles.container}>
                 <FieldSet variante="Secondary">
-                    <InputText placeholder="Buscar filmes"/>
-                    <Botao variant="icon">
+                    <InputText value={searchTerm} onChange={ (e: React.ChangeEvent<HTMLInputElement>) => setSearchTerm(e.target.value)} placeholder="Buscar filmes"/>
+                    <Botao variant="icon" onClick={handleSearch}>
                         <FaSearch/>
                     </Botao>
                 </FieldSet>
                 <h1 className={styles.titulo}>Em cartaz</h1>
-                <MovieList movies={movies}/>
+                {isLoading && <p>Carregando filmes...</p>}
+                {error && <p className={styles.error}>{error}</p>}
+                <MovieList movies={filteredMovies}/>
             </section>
         </main>
     )
