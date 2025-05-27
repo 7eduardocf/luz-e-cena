d8d184d92d6220b11f3280f03d1bb03569c7dce5
7eduardocf <eduardocfrota901@gmail.com>
2025-05-26 23:11:52 -0400

    Resolvido o problema de busca da API

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,9 +1,9 @@
-import axios from "axios";
-import type { Movie } from "../types";
+import axios from "axios"
+import type { Movie } from "../types"
 
-const BASE_URL = "http://localhost:3001/"
+const BASE_URL = "/api"
 
-export const getMovies = async(): Promise<Movie[]> =>{
-    const response = await axios.get<Movie[]>(`${BASE_URL}/movies`)
-    return response.data
+export const getMovies = async (): Promise<Movie[]> => {
+  const response = await axios.get<Movie[]>(`${BASE_URL}/movies`)
+  return response.data
 }
--- src/hooks/useFetchMovies.ts
+++ src/hooks/useFetchMovies.ts
@@ -2,29 +2,30 @@ import { useEffect, useState } from "react"
 import { getMovies } from "../api"
 import type { Movie } from "../types"
 
-const useFetchMovies = () =>{
-    const [movies, setMovies] = useState<Movie[]>([])
-    const [isLoading, setIsloading] = useState<boolean>(false)
-    const [error, setError] = useState<string | null>(null)
+const useFetchMovies = () => {
+  const [movies, setMovies] = useState<Movie[]>([])
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+  const [error, setError] = useState<string | null>(null)
 
-    const fetchMovies = async ()=>{
-        setIsloading(true)
-        setError(null)
-        try {
-            const movies = await getMovies()
-            setMovies(movies)
-        } catch(e){
-            setError("Erro ao buscar filmes, tente novamente")
-            console.error(e)
-        }finally{
-            setIsloading(false)
-        }
+  const fetchMovies = async () => {
+    setIsLoading(true)
+    setError(null)
+    try {
+      const movies = await getMovies()
+      setMovies(movies)
+    } catch (e) {
+      setError("Erro ao buscar filmes, tente novamente")
+      console.error(e)
+    } finally {
+      setIsLoading(false)
     }
+  }
 
-    useEffect(() =>{
-        fetchMovies()
-    },[])
-    return{movies, isLoading, error}
+  useEffect(() => {
+    fetchMovies()
+  }, [])
+
+  return { movies, isLoading, error }
 }
 
 export default useFetchMovies
